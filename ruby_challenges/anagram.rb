a7edd06970a1044c420ee15fc38c405c6b4726bc
Charles A. Hartman <charles@hartman.dev>
2021-01-01 08:10:56 -0800

    working ruby challenges e1.6

--- ruby_challenges/anagram.rb
+++ ruby_challenges/anagram.rb
@@ -10,14 +10,52 @@
 =begin --> PEDAC
 
 problem/rules:
-  -
+  - given an object instantiated with a given string, match any elements that
+    are anagrams that that string from an array passed to a custom #match
+    method and return and return the match(es) as a list
 
-input:
-output:
+input: string, array of strings
+output: array of one or more strings
 
 data structure:
+  - class Anagram defined with one parameter (key_string)
+  - initialize method assigns passed string to instance variable
+  - define #match method that takes array of strings and returns array of
+    strings that are anagrams of the object's (key_string)
 
 algorithm:
-  -
+  - intialize (result) to []
+  - iterate through given array of strings
+    - if string is anagram of (key_string)
+      - iterate through each char of given string
+        - if all? char.count == (key_string).count(char)
+          - then add string to (result)
+  - return (result)
 
 =end
+
+class Anagram
+  def initialize(key_str)
+    @key_str = key_str
+  end
+
+  def match(input)
+    result = []
+    list = filter_input(input)
+
+    list.each do |str|
+      result << str if str.chars.all? do |c|
+        str.downcase.count(c.downcase) == @key_str.downcase.count(c.downcase)
+      end
+    end
+    result
+  end
+
+  private
+
+  def filter_input(arr)
+    arr.select do |str|
+      str.length == @key_str.length && str.downcase != @key_str.downcase
+    end
+  end
+end
