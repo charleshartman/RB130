c8d8c494784df13b02fc254c4e4fa65d343a91db
Charles A. Hartman <charles@hartman.dev>
2021-01-04 20:46:15 -0800

    playing a bit

--- roller/roller.rb
+++ roller/roller.rb
@@ -1,29 +1,33 @@
-# test_tube.rb
+# roller.rb
 
-def report(rolls)
-  puts "You rolled: #{rolls}"
-  puts "Dropping lowest roll...#{rolls.sort!.shift}"
-  sleep 0.50
-  puts "Your remaining rolls #{rolls} total: #{rolls.sum}."
-end
+class RollDice
+  attr_reader :rolls, :sided
+  attr_accessor :result
 
-def roll(roll_times, d_size)
-  puts "Rolling #{roll_times}d#{d_size}..."
-  # sleep 0.50
-  rolls = []
-  while roll_times > 0
-    rolls << rand(1..d_size)
-    yield(rolls.last) if block_given?
-    roll_times -= 1
-   # sleep 0.50
+  def initialize(rolls, sided)
+    @rolls = rolls
+    @sided = sided
+    @result = []
   end
-  report(rolls)
+
+  def roll
+    puts "\nRolling #{rolls}d#{sided}..."
+    chance
+    puts "You rolled: #{result}\n\n"
+    self
+  end
+
+  def chance
+    rolls.times { result << rand(1..sided) }
+  end
+
+  def drop
+    puts "Dropping lowest roll and totaling..."
+    sleep 0.75
+    result.sort!.shift
+    puts "Your total is #{result.sum}"
+  end
 end
 
-def abilities
-  # 6.times { roll(4, 6) { |last| puts "You rolled a #{last}." } }
-  6.times { roll(4, 6) }
-end
-
-abilities
-
+odin = RollDice.new(4, 6)
+odin.roll.drop
